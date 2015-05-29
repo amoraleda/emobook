fa336c179026a6e14af48e3e6065ee9dc3176667
amoraleda <alvaro.moraleda@gmail.com>
2015-05-29 13:17:39 +0200

    Added MongoLAB

--- config/connections.js
+++ config/connections.js
@@ -56,10 +56,11 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  localMongoDB: {
+  mongolabMongoDB: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
+    url: process.env.MONGOLAB_URI
+    // host: 'localhost',
+    // port: 27017,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
